f291f51ef4526dc90f03244932b1e21e6a683b1c
Leonardo Almeida <leonardo.teles.almeida@gmail.com>
2020-05-29 02:40:37 -0300

    Configuração Externalizada: Application Properties

--- src/main/java/br/com/curso/VendasApplication.java
+++ src/main/java/br/com/curso/VendasApplication.java
@@ -1,7 +1,6 @@
 package br.com.curso;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -11,17 +10,13 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class VendasApplication {
 
-	@Autowired
-	@Qualifier("applicationName")
+	@Value("${spring.application.name}")
 	private String applicationName;
 	
 	@GetMapping("/hello")
 	public String helloWorld() {
 		return applicationName;
 	}
-	
-	
-	
 	
 	public static void main(String[] args) {
 		SpringApplication.run(VendasApplication.class, args);
