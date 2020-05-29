f7a4d7011450390a59b625f574750cf02797b725
Leonardo Almeida <leonardo.teles.almeida@gmail.com>
2020-05-29 01:29:30 -0300

    Hello World com Spring Boot

--- src/main/java/br/com/curso/VendasApplication.java
+++ src/main/java/br/com/curso/VendasApplication.java
@@ -2,11 +2,19 @@ package br.com.curso;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class VendasApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(VendasApplication.class, args);
+	}
+	
+	@GetMapping("/hello")
+	public String helloWorld() {
+		return "hello world";
 	}
 }
