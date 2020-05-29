3afda1309d930421a3e01947e366171ab2518eb0
Leonardo Almeida <leonardo.teles.almeida@gmail.com>
2020-05-29 02:52:18 -0300

    Trabalhando com Múltiplos Ambientes e Profiles

--- src/main/java/br/com/curso/MyConfiguration.java
+++ src/main/java/br/com/curso/MyConfiguration.java
@@ -1,13 +1,18 @@
 package br.com.curso;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
 
 @Configuration
+@Profile("development")
 public class MyConfiguration {
-
-	@Bean(name = "applicationName")
-	public String applicationName() {
-		return "Sistema de Vendas";
+	
+	@Bean
+	public CommandLineRunner executar() {
+		return args -> {
+			System.out.println("Rodando a configuração de desenvolvimento");
+		};
 	}
 }
