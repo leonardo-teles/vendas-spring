be2bf3fb1acd8c9b37a8d05ac8afe220f126567b
Leonardo Almeida <leonardo.teles.almeida@gmail.com>
2020-05-29 23:02:31 -0300

    Salvando e Recuperando Clientes

--- src/main/java/br/com/curso/VendasApplication.java
+++ src/main/java/br/com/curso/VendasApplication.java
@@ -1,11 +1,30 @@
 package br.com.curso;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import br.com.curso.domain.Cliente;
+import br.com.curso.repository.Clientes;
 
 @SpringBootApplication
 public class VendasApplication {
 
+	@Bean
+	public CommandLineRunner init(@Autowired Clientes clientes) {
+		return args -> {
+			clientes.salvar(new Cliente("Leonardo"));
+			clientes.salvar(new Cliente("Manuela"));
+			
+			List<Cliente> todosClientes = clientes.obterTodos();
+			todosClientes.forEach(System.out::println);
+		};
+	}
+	
 	public static void main(String[] args) {
 		SpringApplication.run(VendasApplication.class, args);
 	}
