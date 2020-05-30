5208b9efc8804110b0a0a08f1e1847b63cc41342
Leonardo Almeida <leonardo.teles.almeida@gmail.com>
2020-05-29 22:28:26 -0300

    Scripts de Criação das Tabelas do Banco de Dados

---
+++ src/main/resources/data.sql
@@ -0,0 +1,24 @@
+CREATE TABLE clientes(
+	id INTEGER PRIMARY KEY AUTO_INCREMENT,
+	nome VARCHAR(100)
+);
+
+CREATE TABLE produtos(
+	id INTEGER PRIMARY KEY AUTO_INCREMENT,
+	descricao VARCHAR(100),
+	preco_unitario NUMERIC(10,2)
+);
+
+CREATE TABLE pedidos(
+	id INTEGER PRIMARY KEY AUTO_INCREMENT,
+	data_pedido TIMESTAMP,
+	total NUMERIC(10,2),
+	cliente_id INTEGER REFERENCES clientes(id)
+);
+
+CREATE TABLE itens_pedido(
+	id INTEGER PRIMARY KEY AUTO_INCREMENT,
+	quantidade INTEGER,
+	pedido_id INTEGER REFERENCES pedidos(id),
+	produto_id INTEGER REFERENCES produtos(id)
+);
