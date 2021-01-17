e0a05f5ec968d4329800453acd8a35b475faf6a4
Leonardo Almeida <leonardo.teles.almeida@gmail.com>
2021-01-16 23:58:12 -0300

    Criando Repositórios da Entidades

---
+++ src/main/java/br/com/curso/repository/Produtos.java
@@ -0,0 +1,11 @@
+package br.com.curso.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import br.com.curso.domain.Produto;
+
+@Repository
+public interface Produtos extends JpaRepository<Produto, Integer> {
+
+}
