5a54034b952ca6a32bcecf9b5585de171f816486
Leonardo Almeida <leonardo.teles.almeida@gmail.com>
2021-01-17 22:54:50 -0300

    Requisição POST e Request Body

--- src/main/java/br/com/curso/controller/ClienteController.java
+++ src/main/java/br/com/curso/controller/ClienteController.java
@@ -6,6 +6,8 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 import br.com.curso.domain.Cliente;
@@ -30,8 +32,13 @@ public class ClienteController {
 		}
 		
 		return ResponseEntity.notFound().build();
+	}
+	
+	@PostMapping("/api/clientes")
+	@ResponseBody
+	public ResponseEntity<Cliente> save(@RequestBody Cliente cliente) {
+		Cliente clienteSalvo = clientes.save(cliente);
 		
-		
-		
+		return ResponseEntity.ok(clienteSalvo);
 	}
 }
