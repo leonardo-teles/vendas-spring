f0e17303887a3db8185f846ef2ea845342023ba5
Leonardo Almeida <leonardo.teles.almeida@gmail.com>
2021-01-17 23:02:08 -0300

    Delete Mapping: Deletando um Recurso no Servidor

--- src/main/java/br/com/curso/controller/ClienteController.java
+++ src/main/java/br/com/curso/controller/ClienteController.java
@@ -4,6 +4,7 @@ import java.util.Optional;
 
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -34,11 +35,25 @@ public class ClienteController {
 		return ResponseEntity.notFound().build();
 	}
 	
-	@PostMapping("/api/clientes")
 	@ResponseBody
+	@PostMapping("/api/clientes")
 	public ResponseEntity<Cliente> save(@RequestBody Cliente cliente) {
 		Cliente clienteSalvo = clientes.save(cliente);
 		
 		return ResponseEntity.ok(clienteSalvo);
+	}
+	
+	@ResponseBody
+	@DeleteMapping("/api/clientes/{id}")
+	public ResponseEntity<Cliente> delete(@PathVariable Integer id) {
+		Optional<Cliente> cliente = clientes.findById(id);
+		
+		if (cliente.isPresent()) {
+			clientes.delete(cliente.get());
+			
+			return ResponseEntity.noContent().build();
+		}
+
+		return ResponseEntity.notFound().build();
 	}
 }
