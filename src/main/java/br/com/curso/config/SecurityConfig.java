ddfbd4d216a6a8149af8ce8fd03b7a4a7892c864
Leonardo Almeida <leonardo.teles.almeida@gmail.com>
2021-01-22 20:32:17 -0300

    Testando a Autenticação com Cadastro de Usuários

--- src/main/java/br/com/curso/config/SecurityConfig.java
+++ src/main/java/br/com/curso/config/SecurityConfig.java
@@ -2,6 +2,7 @@ package br.com.curso.config;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
@@ -37,6 +38,8 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 				.antMatchers("/api/clientes/**").hasAnyRole("USER", "ADMIN")
 				.antMatchers("/api/pedidos/**").hasAnyRole("USER", "ADMIN")
 				.antMatchers("/api/produtos/**").hasRole("ADMIN")
+				.antMatchers(HttpMethod.POST, "/api/usuarios/**").permitAll()
+				.anyRequest().authenticated()
 			.and()
 				.httpBasic();
 		;
