6dd19baf98ebe613b39a06e211dacc6c6cb6855b
Leonardo Almeida <leonardo.teles.almeida@gmail.com>
2021-01-22 19:46:04 -0300

    Autenticação Basic

--- src/main/java/br/com/curso/config/SecurityConfig.java
+++ src/main/java/br/com/curso/config/SecurityConfig.java
@@ -35,7 +35,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 				.antMatchers("/api/pedidos/**").hasAnyRole("USER", "ADMIN")
 				.antMatchers("/api/produtos/**").hasRole("ADMIN")
 			.and()
-				.formLogin()
+				.httpBasic();
 		;
 	}
 }
