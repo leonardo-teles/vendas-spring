a64c9ad70886e3eff6baa6cb7ccc91db04835202
Leonardo Almeida <leonardo.teles.almeida@gmail.com>
2021-01-23 23:16:06 -0300

    Finalizando a Configuração do JWT no Spring Security

--- src/main/java/br/com/curso/config/SecurityConfig.java
+++ src/main/java/br/com/curso/config/SecurityConfig.java
@@ -7,9 +7,14 @@ import org.springframework.security.config.annotation.authentication.builders.Au
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.web.filter.OncePerRequestFilter;
 
+import br.com.curso.security.jwt.JwtAuthFilter;
+import br.com.curso.security.jwt.JwtService;
 import br.com.curso.service.impl.UsuarioServiceImpl;
 
 @EnableWebSecurity
@@ -18,10 +23,18 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 	@Autowired
 	private UsuarioServiceImpl usuarioService;
 	
+	@Autowired
+    private JwtService jwtService;
+	
 	@Bean
 	public PasswordEncoder passwordEncoder() {
 		return new BCryptPasswordEncoder();
 	}
+	
+	@Bean
+    public OncePerRequestFilter jwtFilter(){
+        return new JwtAuthFilter(jwtService, usuarioService);
+    }
 
 	@Override
 	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
@@ -41,7 +54,10 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 				.antMatchers(HttpMethod.POST, "/api/usuarios/**").permitAll()
 				.anyRequest().authenticated()
 			.and()
-				.httpBasic();
+				.sessionManagement()
+				.sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+			.and()
+            	.addFilterBefore(jwtFilter(), UsernamePasswordAuthenticationFilter.class);
 		;
 	}
 }
