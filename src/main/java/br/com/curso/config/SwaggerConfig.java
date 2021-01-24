d94027821681fc431908089b47454a037f971c16
Leonardo Almeida <leonardo.teles.almeida@gmail.com>
2021-01-24 18:09:17 -0300

    Configurando o Swagger

---
+++ src/main/java/br/com/curso/config/SwaggerConfig.java
@@ -0,0 +1,42 @@
+package br.com.curso.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.Contact;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+@Configuration
+@EnableSwagger2
+public class SwaggerConfig {
+
+	@Bean
+    public Docket docket(){
+        return new Docket(DocumentationType.SWAGGER_2)
+                .useDefaultResponseMessages(false)
+                .select()
+                .apis(RequestHandlerSelectors.basePackage("br.com.curso.controller"))
+                .paths(PathSelectors.any())
+                .build()
+                .apiInfo(apiInfo());
+    }
+	
+	private ApiInfo apiInfo(){
+        return new ApiInfoBuilder()
+                .title("Vendas API")
+                .description("Api do Projeto de Vendas")
+                .version("1.0")
+                .contact(contact())
+                .build();
+    }
+	
+	private Contact contact(){
+        return new Contact("Leonardo Almeida", "http://github.com/leonardo-teles", "leonardo.teles.almeida@gmail.com");
+    }
+}
